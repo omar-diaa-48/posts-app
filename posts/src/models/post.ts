7607d5aa6c4bab2c0546bc1335e36003d8386bb4
Omar <omar@kamelizer.com>
2021-11-13 00:31:41 +0200

    add post model

---
+++ posts/src/models/post.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+interface PostAttributes {
+    title:string;
+    content:string;
+    owner:string;
+}
+
+interface PostDocument extends mongoose.Document{
+    title:string;
+    content:string;
+    owner:string;   
+}
+
+interface PostModel extends mongoose.Model<PostDocument>{
+    build(attributes:PostAttributes):PostDocument;
+}
+
+const postSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    content:{
+        type:String,
+        required:true
+    },
+    owner:{
+        type:String,
+        required:true
+    },
+}, {
+    timestamps:true,
+    toJSON:{
+        transform(doc, ret){
+            ret.id = ret._id
+        }
+    }
+})
+
+const Post = mongoose.model<PostDocument, PostModel>('Post', postSchema)
+
+postSchema.statics.build = (attributes:PostAttributes) => {
+    return new Post(attributes);
+}
+
+export default Post;
