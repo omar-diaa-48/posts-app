a975fe6e90a6f41ec13cd96d0edd323ff14a895f
Omar <omar@kamelizer.com>
2021-11-13 00:23:49 +0200

    connect to mongo db

--- posts/src/index.ts
+++ posts/src/index.ts
@@ -1,13 +1,25 @@
 import express, { Request, Response } from "express";
 import app from './app'
+import mongoose from "mongoose";
 
 import dotenv from 'dotenv'
 dotenv.config();
 
 const start = async () => {
 
-    const port = process.env.PORT
-    
+    if(!process.env.MONGO_URI){
+        throw new Error('MONGO_URI must be defined')
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('Mongo db is connected');
+    } catch (error) {
+        console.error(error);
+    }
+
+    const port = process.env.PORT || 3000
+
     app.listen(port, () => {
         console.log('Posts service started....');
     });
