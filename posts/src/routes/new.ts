e971139a38898ac3b936411fee87c5c40d42d3c7
Omar <omar@kamelizer.com>
2021-11-20 17:17:16 +0200

    adjust auth service

--- posts/src/routes/new.ts
+++ posts/src/routes/new.ts
@@ -2,25 +2,26 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { validateRequest } from "../middlewares/validate-request";
 import Post from "../models/post";
+import { requireAuth } from "../middlewares/require-auth"
 
 const router = express.Router();
 
 router.post(
-    '/api/posts', 
+    '/api/posts',
+    requireAuth, 
     [
         body('title').not().isEmpty().withMessage('Title must be provided').isString().withMessage('Title must be text'),
         body('content').not().isEmpty().withMessage('Content must be provided').isString().withMessage('Content must be text'),
-        body('owner').not().isEmpty().withMessage('Owner of the post must be provided').isString().withMessage('Owner must be text'),
     ],
     validateRequest,
     async (req:Request, res:Response) => {
     
-    const {title, content, owner} = req.body;        
+    const {title, content} = req.body;        
 
     const post = Post.build({
         title,
         content,
-        owner
+        owner:req.currentUser!.id
     });
 
     await post.save();
